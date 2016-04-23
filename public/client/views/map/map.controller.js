94a580c3b284e0dc35fa536d50ccb9bfc046ff3a
gxless <hitgx@yahoo.com>
2016-04-23 16:43:36 -0400

    updated project implementation

--- public/client/views/map/map.controller.js
+++ public/client/views/map/map.controller.js
@@ -3,11 +3,12 @@
         .module("DoMeAFavorApp")
         .controller("MapController", MapController);
 
-    function MapController(FavorService) {
+    function MapController($scope, FavorService, MapService) {
 
         var initCenter = {lat: 42.348, lng: -71.064};
-        var initMap = {center: initCenter, zoom: 12};
-        var map = new google.maps.Map(document.getElementById("googleMap"), initMap);
+        $scope.setPlace = setPlace;
+
+        init(initCenter);
 
         function makeInfoWindowEvent(map, infoWindow, marker) {
             return function() {
@@ -15,32 +16,48 @@
             };
         }
 
-        FavorService.getFavorsByTagId(0)
-            .then(function (response) {
-                var favors = response;
-
-                //display markers after retrieving positions
-                var markers = [];
-                var infoWindows = [];
-
-                for(var i in favors) {
-                    markers[i] = new google.maps.Marker({
-                        map: map,
-                        position: favors[i].position
+        function setPlace(address) {
+            if(address) {
+                MapService.getPosition(address)
+                    .then(function (response) {
+                        init(response);
                     });
+            }
+        }
 
-                    var link = "#/"+favors[i].coordinatorId+"/favors/"+favors[i]._id;
-                    var title = favors[i].title;
-                    infoWindows[i] = new google.maps.InfoWindow({
-                        content: "<a href=" + link + ">"+title+"</a>"
-                    });
+        function init(center) {
+            var initCenter = center;
+            var initMap = {center: initCenter, zoom: 13};
+            var map = new google.maps.Map(document.getElementById("googleMap"), initMap);
 
-                    google.maps.event.addListener(markers[i], "click",
-                        makeInfoWindowEvent(map, infoWindows[i], markers[i]));
-                }
+            FavorService.getFavorsByTagId(0)
+                .then(function (response) {
+                    var favors = response;
+
+                    //display markers after retrieving positions
+                    var markers = [];
+                    var infoWindows = [];
+
+                    for(var i in favors) {
+                        markers[i] = new google.maps.Marker({
+                            map: map,
+                            position: favors[i].position
+                        });
+
+                        var link = "#/"+favors[i].coordinatorId+"/favors/"+favors[i]._id;
+                        var title = favors[i].title;
+                        infoWindows[i] = new google.maps.InfoWindow({
+                            content: "<a href=" + link + ">"+title+"</a>"
+                        });
+
+                        google.maps.event.addListener(markers[i], "click",
+                            makeInfoWindowEvent(map, infoWindows[i], markers[i]));
+                    }
 
 
-            });
+                });
+        }
+
 
 
     }
